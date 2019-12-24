fd369451b3e3179c2e39f22ca66643c2fde31b02
Sandy Gifford <sanford.r.gifford@gmail.com>
2019-12-23 22:06:02 -0500

    prep for github pages

--- dist/server/routing.ts
+++ dist/server/routing.ts
@@ -6,10 +6,7 @@ const app = express();
 const rootDir = process.cwd();
 const distDir = path.resolve(rootDir, "dist");
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.sendFile(path.join(distDir, "index.html")));
 app.get(/^\/(assets|build)\/.*/, (req, res) => res.sendFile(path.join(distDir, req.url)));
 
 export default app;
