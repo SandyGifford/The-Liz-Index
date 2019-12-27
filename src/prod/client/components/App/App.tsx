10c2b67794906a75cf53b0b1ad601e9b2f48e77b
Sandy Gifford <sanford.r.gifford@gmail.com>
2019-12-26 19:41:13 -0500

    sidebar

--- src/prod/client/components/App/App.tsx
+++ src/prod/client/components/App/App.tsx
@@ -2,6 +2,7 @@ import "./App.style";
 
 import * as React from "react";
 import SetPickerPage from "../pages/SetPickerPage/SetPickerPage";
+import DOMUtils from "@utils/DOMUtils";
 
 export interface AppProps { }
 export interface AppState { }
@@ -16,7 +17,24 @@ export default class App extends React.PureComponent<AppProps, AppState> {
 		return (
 			<div className="App">
 				<div className="App__sidebarBackground" />
-				<div className="App__sidebarContent"></div>
+				<div className="App__sidebarContent">
+					<div className={DOMUtils.getBEMClassName("App__sidebarContent__item", { selected: true })}>
+						<div className="App__sidebarContent__item__label">Track</div>
+						<div className="App__sidebarContent__item__sub">manually enter cards</div>
+					</div>
+					<div className={DOMUtils.getBEMClassName("App__sidebarContent__item", { disabled: true })}>
+						<div className="App__sidebarContent__item__label">Play</div>
+						<div className="App__sidebarContent__item__sub">not currently available</div>
+					</div>
+					<div className={DOMUtils.getBEMClassName("App__sidebarContent__item", { disabled: true })}>
+						<div className="App__sidebarContent__item__label">Shape Designer</div>
+						<div className="App__sidebarContent__item__sub">not currently available</div>
+					</div>
+					<div className={DOMUtils.getBEMClassName("App__sidebarContent__item", { disabled: true })}>
+						<div className="App__sidebarContent__item__label">Shade Designer</div>
+						<div className="App__sidebarContent__item__sub">not currently available</div>
+					</div>
+				</div>
 				<div className="App__toolbar"></div>
 				<div className="App__content">
 					<SetPickerPage />
